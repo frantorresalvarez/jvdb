268dda573178cd8212b9158e9e2a53ae842065f9
frantorresalvarez <franciscotorresdj@gmail.com>
2024-02-04 09:40:40 +0100

    guardar datos en archivo

--- jvdb.c
+++ jvdb.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
+    FILE *archivo;
+    archivo = fopen("basededatos.txt","w");
+    char *texto = "JVDB";
+    fputs(texto,archivo);
+    fclose(archivo);
     printf("JVDB");
     return 0;
 }
